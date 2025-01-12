76aeef9adaa6a44ea712e2cf7ddd88efdae79778
Umar Farooq <uf29664@gmail.com>
2025-01-12 22:11:31 +0500

    mainPage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { AppBar, Box, styled, Toolbar } from '@mui/material';
+import { AppBar, styled, Box, Typography } from '@mui/material';
 import './App.css';
+import whatsapp from './images/whatsapp.svg'
+import LoginDetails from './components/account/loginDetails/LoginDetails';
 
 
 const Header = styled(AppBar)`
 height:200px;
-background:green;
+background:#25D366;
 `
 function App() {
   return (
    <>
    <Header>
-    <Toolbar>
-
-    </Toolbar>
+    <Box className='flex justify-start items-center pl-20 pt-6'>
+      <img src={whatsapp} className='' width={30} height={30} alt='whatsapp pic'/>
+      <Typography className='font-bold text-3xl pl-4'>Web Whatsapp</Typography>
+    </Box>
+    <LoginDetails/>
    </Header>
 
    </>
--- src/components/account/loginDetails/LoginDetails.js
+++ src/components/account/loginDetails/LoginDetails.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import Dialog from '@mui/material/Dialog';
+import { Box, List, ListItem, Typography } from '@mui/material';
+import {qrCodeImage} from '../../../constants/Constants'
 export default function LoginDetails() {
   return (
-    <div>
-      
+    <div >
+     
+     <Dialog className='w-100%'
+     open={true}>
+      <Box className='grid md:grid-cols-2'>
+        <Box>
+      <Typography>Log into WhatsApp Web</Typography>
+<List>
+  <ListItem>1. Opent Whatsapp on your phone :</ListItem>
+  <ListItem>2. Tap <Typography component='span'> </Typography> or <Typography component='span'>setting : </Typography> and select <Typography component='span'>Linked Devices </Typography></ListItem>
+  <span className="">Menu :</span>
+  <span className="">Menu :</span>
+  <span className="">Menu :</span>
+  <ListItem>3. Scan this QR code</ListItem>
+</List>
+        </Box>
+        <Box>
+          <img src={qrCodeImage} alt="scan code" />
+        </Box>
+
+      </Box>
+     </Dialog>
     </div>
   )
 }
