8d99cd0ec6a7f9e36c391a074f30be28359b2e28
Umar Farooq <uf29664@gmail.com>
2025-01-12 22:46:30 +0500

    scanner

--- src/components/account/loginDetails/LoginDetails.js
+++ src/components/account/loginDetails/LoginDetails.js
@@ -1,26 +1,41 @@
 import React from 'react'
 import Dialog from '@mui/material/Dialog';
-import { Box, List, ListItem, Typography } from '@mui/material';
+import { Box, List, ListItem,  Typography } from '@mui/material';
 import {qrCodeImage} from '../../../constants/Constants'
+// const log = styled(Typography)`
+// fontSize:25px
+// `;
+const dialogStyle = {
+  height:"70%",
+  // minWidth:'800px',
+  boxShadow:'none',
+  // maxWidth:'95%',
+  overflow:'hidden',
+  width:"clamp(900,80%)"
+
+}
+
 export default function LoginDetails() {
   return (
     <div >
      
-     <Dialog className='w-100%'
-     open={true}>
+     <Dialog className='min-w-[90%]'
+     open={true} PaperProps={{sx:dialogStyle}}>
       <Box className='grid md:grid-cols-2'>
-        <Box>
-      <Typography>Log into WhatsApp Web</Typography>
+        <Box className='p-8'>
+      <Typography sx={{fontSize:'25px',fontWeight:'bold'}} >Log into WhatsApp Web</Typography>
 <List>
   <ListItem>1. Opent Whatsapp on your phone :</ListItem>
-  <ListItem>2. Tap <Typography component='span'> </Typography> or <Typography component='span'>setting : </Typography> and select <Typography component='span'>Linked Devices </Typography></ListItem>
-  <span className="">Menu :</span>
-  <span className="">Menu :</span>
-  <span className="">Menu :</span>
+  <ListItem>2. Tap 
+
+  Menu : or
+setting :
+  Linked Devices :
+  </ListItem>
   <ListItem>3. Scan this QR code</ListItem>
 </List>
         </Box>
-        <Box>
+        <Box className='p-8'>
           <img src={qrCodeImage} alt="scan code" />
         </Box>
 
