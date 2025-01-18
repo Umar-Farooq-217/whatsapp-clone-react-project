e593688c586c80225b36bb824860033ca7014aaa
Umar Farooq <uf29664@gmail.com>
2025-01-18 21:48:41 +0500

    chat

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,9 +1,34 @@
+import { Box, Dialog, styled } from '@mui/material'
 import React from 'react'
 
 export default function Chat() {
+  const Component = styled(Dialog)`
+  
+  background-color: #00A884;
+  height:150px;
+  width:100%
+
+  `
+  // const mainBox = {
+  //   minHeight:'100%',
+  //   display:'flex',
+
+  // }
+  
   return (
-    <div>
-      hello from chat
-    </div>
+    <>
+    <main>
+    <Component open={true} hideBackdrop={true} maxWidth={false} fullWidth={true}>
+      <mainBox PaperProps={{sx:mainBox}} >
+
+      <Box>
+        left Box 
+      </Box>
+      <Box>
+Right Box
+      </Box></mainBox>
+    </Component>
+     
+    </main></>
   )
 }
