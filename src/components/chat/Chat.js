1eef16a3bc23e042d3c81a842620e26c281060d0
Umar Farooq <uf29664@gmail.com>
2025-01-19 21:12:15 +0500

    header

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,31 +1,35 @@
-import { Box, Dialog } from '@mui/material'
+import { AppBar, Box, Dialog, Toolbar } from '@mui/material'
 import React from 'react'
+import Header from './Header'
+
 const dialogStyle = {
-  height:"80%",
-  minWidth:'95%',
-  // minWidth:'800px',
-  boxShadow:'5px',
-  maxWidth:'1500px',
-  // overflow:'hidden',
-  // width:"clamp(900px,90%)",
-  marginTop:'45px'
+ height:"100%",
+ width:'100%',
+ marginTop:'15px',
+ marginLeft:'15px',
+ marginRight:'15px',
+ display:'flex'
+
 
 }
 export default function Chat() {
-  <Dialog
-  open={true} PaperProps={{sx:dialogStyle}} hideBackdrop={true} maxWidt={false} >
-   <Box className='grid md:grid-cols-2 w-full'>
-    
-     </Box>
-     <Box sx={{padding : '32px', margin:'auto'}}>
-  
-     </Box>
 
-   </Box>
-  </Dialog>
   
   return (
-    <>
-   </>
+    <Box>
+      <AppBar sx={{backgroundColor:'#00A783',height:'125px'}}>
+        <Toolbar>
+
+        </Toolbar>
+      </AppBar>
+      <Dialog open={true} PaperProps={{sx:dialogStyle}} hideBackdrop={true} maxWidth={false}>
+        <Box className='flex max-w-[1500px]'> 
+      <Box className='min-w-[400px]'>
+       <Header/>
+      </Box>
+      <Box className='min-w-[300px]'>right box</Box></Box>
+      </Dialog>
+
+   </Box>
   )
 }
