418a33c7000ef7e788d361559189c3a62dde6a6c
Umar Farooq <uf29664@gmail.com>
2025-01-22 21:35:08 +0500

    drawer

--- src/components/chat/HeaderMenu.js
+++ src/components/chat/HeaderMenu.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {Box , Menu , MenuItem} from '@mui/material'
 export default function HeaderMenu() {
+  const [open,setOpen] = useState(null)
+  const handleClick = (e)=>{
+    setOpen(e.currentTarget)
+  }
+  const handleClose = ()=>{
+    setOpen(null)
+  }
+  
   return (
-    <div>
-      
-    </div>
+    <Box >
+      <MoreVertIcon onClick={handleClick} />
+      <Menu open={open} onClose={handleClose} keepMounted anchorEl={open} anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center'
+      }}
+      transformOrigin={{
+        horizontal:'right',
+        vertical:'top',
+      }}
+      >
+        <MenuItem sx={{marginTop:'6px'}}>Profile</MenuItem>
+      </Menu>
+    </Box>
   )
 }
