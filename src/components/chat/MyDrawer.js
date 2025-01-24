df5eb32413ad40f0b6fa070a77d6fc80f406f9c1
Umar Farooq <uf29664@gmail.com>
2025-01-24 22:02:37 +0500

    database

--- src/components/chat/MyDrawer.js
+++ src/components/chat/MyDrawer.js
@@ -12,15 +12,24 @@ export default function MyDrawer({open, setOpen}) {
   }
   return (
     
-      <Drawer hideBackdrop={true} className='' style={{zIndex:1500}} PaperProps={{sx:drawerStyle}} open={open} onClose={handleClose}>
+      <Drawer  className='' style={{zIndex:1500}} PaperProps={{sx:drawerStyle}} open={open} onClose={handleClose}>
         <Box  sx={{backgroundColor:'#00A783',height:'102px',color:'white',display:'flex',gap:5 ,alignItems:'end',paddingLeft:5,paddingBottom:2}}>
           <ArrowBackIcon sx={{fontSize:'25px',fontWeight:600}}/>
           <Typography sx={{fontSize:"16px",fontWeight:600}}>Profile</Typography>
         </Box>
-        <Box sx={{display:'flex',justifyContent:'center',padding:'32px 0px' ,backgroundColor:'#F0F2F5'}}>
-        <img src={account.picture} alt="dp" className='w-[150px] rounded-full' />
+        <Box sx={{display:'flex',justifyContent:'center',padding:'25px 0px' ,backgroundColor:'#F0F2F5'}}>
+        <img src={account.picture} alt="dp" className='w-[130px] rounded-full' />
 
         </Box>
+        <Box className='shadow-lg px-6 py-4'>
+          <Typography className='text-[#00A783] font-semibold font-sans'>Your Name </Typography>
+          <Typography className='font-bold pt-3'>{account.name}</Typography>
+        </Box>
+        <Box>
+          <Typography className='text-gray-500 text-sm px-6 py-4 '>This is not your UserName or Pin . This name will be visible to your Whatsapp Contacts</Typography>
+
+        </Box>
+        <Box></Box>
       </Drawer>
     
   )
