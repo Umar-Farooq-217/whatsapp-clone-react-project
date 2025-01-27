a26bd28f072800d4a352f052fc9d2e731104b5be
Umar Farooq <uf29664@gmail.com>
2025-01-27 21:13:05 +0500

    emptyChat

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -4,34 +4,34 @@ import Header from './Header'
 import EmptyChat from '../emptyChat/EmptyChat'
 
 const dialogStyle = {
- height:"100%",
- width:'100%',
- marginTop:'15px',
- marginLeft:'15px',
- marginRight:'15px',
- display:'flex'
+  height: "100%",
+  width: '100%',
+  marginTop: '15px',
+  marginLeft: '15px',
+  marginRight: '15px',
+  display: 'flex'
 
 
 }
 export default function Chat() {
 
-  
+
   return (
     <Box>
-      <AppBar sx={{backgroundColor:'#00A783',height:'125px'}}>
+      <AppBar sx={{ backgroundColor: '#00A783', height: '125px' }}>
         <Toolbar>
 
         </Toolbar>
       </AppBar>
-      <Dialog open={true} PaperProps={{sx:dialogStyle}} hideBackdrop={true} maxWidth={false}>
-        <Box className='flex max-w-[1500px]'> 
-      <Box className='min-w-[400px]'>
-       <Header/>
-      </Box>
-      <EmptyChat/>
-      </Box>
+      <Dialog open={true} PaperProps={{ sx: dialogStyle }} hideBackdrop={true} maxWidth={false}>
+        <Box className='flex max-w-[1500px]'>
+          <Box className='min-w-[400px]'>
+            <Header />
+          </Box>
+          <EmptyChat />
+        </Box>
       </Dialog>
 
-   </Box>
+    </Box>
   )
 }
--- src/components/emptyChat/EmptyChat.js
+++ src/components/emptyChat/EmptyChat.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import {emptyChatImage} from '../../constants/Constants'
-import { Box, Typography } from '@mui/material'
-
+import { Box, Divider, Typography } from '@mui/material'
+import ComputerIcon from '@mui/icons-material/Computer';
+import LockIcon from '@mui/icons-material/Lock';
 export default function EmptyChat() {
   return (
     <Box className='w-full  bg-[#F8FAFB]'>
    <img src={emptyChatImage} alt="EmptyChatImage" className='w-[400px] flex m-auto ' />
-   <Typography sx={{textAlign:'center',fontSize:'34px',color:'#2f2f2e'}}> WhatsApp Web</Typography>
-   <Typography>Now send and recieve messages online without keeping 
-   </Typography>
-      
+   <Typography sx={{textAlign:'center',fontSize:'34px',color:'#41525d'}}> WhatsApp Web</Typography>
+   <Typography sx={{color:'#41525d',textAlign:'center'}}>Now send and recieve messages online without keeping your phone online.</Typography>
+   <Typography sx={{color:'#41525d',textAlign:'center'}}>Use WhatsApp up to 4 linked devices with one phone at same time</Typography>
+      <Divider/>
+      <Box className='flex  justify-center'>
+        <ComputerIcon sx={{color:'#41525d',width:20,marginRight:2}} />
+        <Typography sx={{color:'#41525d',textAlign:'center'}}>Make calls from desktop with WhatsApp for windows. Get it here.</Typography>
+      </Box>
+      <Box className='flex  justify-center'>
+        <LockIcon sx={{color:'#41525d',width:20,marginRight:2}} />
+        <Typography sx={{color:'#41525d',textAlign:'center'}}>End to End encryptedl.</Typography>
+      </Box>
     </Box>
   )
 }
