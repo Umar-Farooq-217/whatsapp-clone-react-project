f8f2a092a87b343e7892f49a047a9dc3db5a1ac1
Umar Farooq <uf29664@gmail.com>
2025-01-27 21:32:47 +0500

    empty

--- src/components/emptyChat/EmptyChat.js
+++ src/components/emptyChat/EmptyChat.js
@@ -6,19 +6,20 @@ import LockIcon from '@mui/icons-material/Lock';
 export default function EmptyChat() {
   return (
     <Box className='w-full  bg-[#F8FAFB]'>
-   <img src={emptyChatImage} alt="EmptyChatImage" className='w-[400px] flex m-auto ' />
-   <Typography sx={{textAlign:'center',fontSize:'34px',color:'#41525d'}}> WhatsApp Web</Typography>
-   <Typography sx={{color:'#41525d',textAlign:'center'}}>Now send and recieve messages online without keeping your phone online.</Typography>
-   <Typography sx={{color:'#41525d',textAlign:'center'}}>Use WhatsApp up to 4 linked devices with one phone at same time</Typography>
-      <Divider/>
-      <Box className='flex  justify-center'>
+   <img src={emptyChatImage} alt="EmptyChatImage" className='w-[400px] flex m-auto pt-24 ' />
+   <Typography sx={{textAlign:'center',fontSize:'30px',color:'#41525d'}}> WhatsApp Web</Typography>
+   <Typography sx={{color:'#41525d',textAlign:'center',paddingTop:2}}>Now send and recieve messages online without keeping your phone online.</Typography>
+   <Typography sx={{color:'#41525d',textAlign:'center'}}>Use WhatsApp on up to 4 linked devices with one phone at same time</Typography>
+      <Divider sx={{width:'50%',display:'flex',margin:'auto',paddingTop:'20px'}} />
+      <Box className='flex  justify-center pt-5'>
         <ComputerIcon sx={{color:'#41525d',width:20,marginRight:2}} />
         <Typography sx={{color:'#41525d',textAlign:'center'}}>Make calls from desktop with WhatsApp for windows. Get it here.</Typography>
       </Box>
-      <Box className='flex  justify-center'>
+      <Box className='flex  justify-center pt-20'>
         <LockIcon sx={{color:'#41525d',width:20,marginRight:2}} />
         <Typography sx={{color:'#41525d',textAlign:'center'}}>End to End encryptedl.</Typography>
       </Box>
+     <Box className='h-2 w-full bg-[#00A783] mt-8'></Box>
     </Box>
   )
 }
