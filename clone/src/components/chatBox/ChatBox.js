b8bb9c4aed0db09e8c38e4693c0dad78807250a0
Umar Farooq <uf29664@gmail.com>
2025-03-05 22:32:12 +0500

    commit

--- clone/src/components/chatBox/ChatBox.js
+++ clone/src/components/chatBox/ChatBox.js
@@ -3,24 +3,11 @@ import Messages from './Messages';
 import { Box } from '@mui/material';
 import { AccountContext } from '../context/AccountData';
 import { getConversation } from '../../services/api';
-// import { getConversation } from '../../services/api';
 
 export default function ChatBox() {
   const { person, account } = useContext(AccountContext);
 
-  // useEffect(() => {
-  //   const getConversationDetails = async () => {
-
-  //     try {
-  //       const  data = await getConversation({ senderId: account.sub, receiverId: person.sub });
-  //       console.log('Conversation Data:', data);
-  //     } catch (error) {
-  //       console.error('Error fetching conversation:', error);
-  //     }
-  //   };
-
-  //   getConversationDetails();
-  // }, [person.sub,account.sub]);
+ 
 
   useEffect(()=>{
     try {
