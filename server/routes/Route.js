5bab441086927c67cb5199860b79a5d9e0eda164
Umar Farooq <uf29664@gmail.com>
2025-03-10 21:58:39 +0500

    getmessage

--- server/routes/Route.js
+++ server/routes/Route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { addUser,getUsers } from '../controller/UserConroller.js';
 import { newConversation , getConversation  } from '../controller/ConversatonsController.js';
-import { newMessage } from '../controller/message-conroller.js';
+import { newMessage , getMessage } from '../controller/message-conroller.js';
 
 const router  = express.Router()
 router.post('/add',addUser)
@@ -9,6 +9,7 @@ router.get('/users',getUsers)
 router.post('/conversation/add',newConversation)
 router.post('/conversation/get', getConversation)
 router.post('/message/add', newMessage)
+router.post('/message/get', getMessage)
 
 
 export default router;
