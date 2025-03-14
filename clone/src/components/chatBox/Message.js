9d7262fdf36a2f0dd3f2234111d2fa9e1318c286
Umar Farooq <uf29664@gmail.com>
2025-03-14 22:23:23 +0500

    nice

--- clone/src/components/chatBox/Message.js
+++ clone/src/components/chatBox/Message.js
@@ -1,33 +1,50 @@
 import { Box, styled, Typography } from '@mui/material'
-import React from 'react'
+import React, { useContext } from 'react'
+import { formatDate } from '../../utils/CommonUtils'
+import  { AccountContext } from '../context/AccountData'
+
+
+
+
+
 const SendMessages =styled(Box)`
     background:#dcf8c6;
     max-width : 60% ;
     margin-left : auto ;
-    padding : 8px ; 
     width : fit-content ; 
     display : flex ; 
     border-radius : 10px ; 
-    word-break : break-word 
+    word-break : break-word ;
+    margin-top: 8px
     
     `
 const ReceivedMessages =styled(Box)`
     background:#FFFFFF;
     max-width : 60% ;
-    margin-left : auto ;
     padding : 8px ; 
     width : fit-content ; 
     display : flex ; 
     border-radius : 10px ; 
     word-break : break-word 
-    
     `
 export default function Message({message}) {
-    
+    const { account } = useContext(AccountContext)
   return (
-    <SendMessages >
-      <Typography className=''>{message.text}</Typography>
-      <Typography>{message.createdAt}</Typography>
+    <>
+    {
+      account.sub === message.senderId ?
+      <SendMessages >
+      <Typography sx={{fontSize:20, paddingLeft:2 , paddingTop:'5px', paddingBottom:'5px'}}>{message.text}</Typography>
+      <Typography sx={{fontSize:14 , marginLeft:2 ,paddingRight:1, display:'flex',marginTop:'auto', wordBreak:'keep-all', color:'gray'}} >{formatDate(message.createdAt)}</Typography>
     </SendMessages>
+    :
+    <ReceivedMessages >
+      <Typography sx={{fontSize:20, paddingLeft:2 , paddingTop:'5px', paddingBottom:'5px'}}>{message.text}</Typography>
+      <Typography sx={{fontSize:14 , marginLeft:2 ,paddingRight:1, display:'flex',marginTop:'auto', wordBreak:'keep-all', color:'gray'}} >{formatDate(message.createdAt)}</Typography>
+    </ReceivedMessages>
+    }
+    </>
+
+    
   )
 }
