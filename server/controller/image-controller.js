9079442353483b72b61015cd171bef63a52b21b7
Umar Farooq <uf29664@gmail.com>
2025-03-28 23:20:05 +0500

    nice

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,11 @@
 
-
+const url = 'http : //localhost:8000'
 
 
 export const uploadFile = async(req , res)=>{
-    
+    if(!req.file){
+        return res.status(404).json('file not found')
+    }
+
+    const imageUrl = `${url}/file/${req.file.fileName}`
 }
--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,7 +1,23 @@
 import multer from 'multer'
+import { GridFsStorage } from "multer-gridfs-storage";
 import dotenv from 'dotenv'
+dotenv.config()
 
-
-const storage = new FormData({
-
+const URL = process.env.DATABASE_URL;
+const storage = new GridFsStorage({
+URL,
+options : { useNewUrlParser: true },
+file : (request , file)=>{
+    const match = [ 'image/png' , 'image/jpg']
+    if(match.indexOf(file.mimeType) === -1){
+        return `${Date.now()}-file-${file.originalName}`
+    }
+    // in case of file
+    return {
+        bucketName : 'photos',
+     fileName :  `${Date.now()}-file-${file.originalName}`
+    }
+}
 })
+
+export default multer({ storage })
