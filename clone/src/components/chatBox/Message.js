a80f8155af4be0f9d2bb1f41cd1bbc1386da3db1
Umar Farooq <uf29664@gmail.com>
2025-04-13 08:39:23 +0500

    imagedownload

--- clone/src/components/chatBox/Message.js
+++ clone/src/components/chatBox/Message.js
@@ -1,6 +1,6 @@
 import { Box, styled, Typography } from '@mui/material'
 import React, { useContext } from 'react'
-import { formatDate } from '../../utils/CommonUtils'
+import { formatDate , downloadMedia } from '../../utils/CommonUtils'
 import { AccountContext } from '../context/AccountData'
 import GetAppIcon from '@mui/icons-material/GetApp';
 import { iconPDF} from '../../constants/Constants'
@@ -72,6 +72,7 @@ const ImageMessage = ({ message }) => {
  <Typography sx={{fontSize:'17px',wordBreak:'keep-all',paddingTop:1,paddingRight:2}}>{message.text.split('/').pop()}</Typography>
  <Box sx={{ fontSize: 17, marginLeft: 3, paddingRight: 1,  marginTop: 'auto', wordBreak: 'keep-all', color: 'gray',position:'absolute',bottom:2 , right:0 }} >
               <GetAppIcon
+              onClick={(e)=>downloadMedia(e,message.text)}
                 style={{ marginRight: 10, border: '1px solid gray', borderRadius: '50%', padding: 1, fontSize:'30px' }} />
               {formatDate(message.createdAt)}</Box>
               </Box>
@@ -81,6 +82,7 @@ const ImageMessage = ({ message }) => {
             <img src={message.text} alt={message.text} style={{ width: '300px', height: '100%', objectFit: 'cover', borderRadius: 8 }} />
             <Box sx={{ fontSize: 14, marginLeft: 3, paddingRight: 1, display: 'flex', marginTop: 'auto', wordBreak: 'keep-all', color: 'gray',position:'absolute' , bottom:0 , right:2 }} >
               <GetAppIcon
+               onClick={(e)=>downloadMedia(e,message.text)}
                 style={{ marginRight: 10, border: '1px solid gray', borderRadius: '50%', padding: 1, fontSize:'30px', marginBottom:4 }} />
               {formatDate(message.createdAt)}</Box>
             </Box>
--- clone/src/utils/CommonUtils.js
+++ clone/src/utils/CommonUtils.js
@@ -7,3 +7,27 @@ export const formatDate = (date)=>{
 
     return `${hours < 10 ? '0' + hours : hours} : ${minutes < 10 ? '0' + minutes : minutes}`
 }
+
+export const downloadMedia = (e,originalImage) =>{
+    e.preventDefault()
+    try {
+        fetch(originalImage)
+        .then(resp => resp.blob())
+        // in case of document we use res.json() but in case of file or image we use blob()
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            const nameSplit = originalImage.split('/')
+            const duplicateName = nameSplit.pop()
+            a.download = '' + duplicateName + '';
+            document.body.appendChild(a)
+            a.click()
+            window.URL.revokeObjectURL(url)
+        }).catch(error =>  console.log('Error while downloading Image' , error.message) )
+    } catch (error) {
+        console.log('Error while downloading Image', error.message);
+        
+    }
+}
